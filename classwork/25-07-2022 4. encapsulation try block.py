c3793061aa08d8a0c7a512af61f1e95f89ebe72c
Alok kumar <edrag112@gmail.com>
2022-08-04 10:39:45 +0530

    Initial commit

---
+++ classwork/25-07-2022 4. encapsulation try block.py
@@ -0,0 +1,22 @@
+class invalidnumber():
+
+    def __init__(self,arg):
+        self.arg=arg
+
+try:
+    num=int(input())
+    if num>20:
+        print("hello world")
+    else:
+        print(num)
+
+except :
+    print("alok kumar")
+
+
+
+
+
+
+
+
